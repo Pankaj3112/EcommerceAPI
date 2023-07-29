efa70c9a42c53861a8732dce0223ee9c6ca2c374
Pankaj <pankajbeniwal3112@gmail.com>
2023-07-29 15:48:16 +0530

    add mongo atlas database

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 //connect to database
 async function main() {
-    const db = await mongoose.connect('mongodb://127.0.0.1:27017/EcommerceAPI');
+    const db = await mongoose.connect('mongodb+srv://pankaj3112:udPAnOOdZKwZxo5w@cluster0.lm6iyo2.mongodb.net/?retryWrites=true&w=majority');
     module.exports = db;
 }
 
