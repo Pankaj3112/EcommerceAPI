aef3481c3944bb2571e9d5779eed80438b8351a6
Pankaj <pankajbeniwal3112@gmail.com>
2023-07-29 16:14:10 +0530

    add atlas db

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 
 //connect to database
 async function main() {
+    //connecting to my atlas DB
     const db = await mongoose.connect('mongodb+srv://pankaj3112:udPAnOOdZKwZxo5w@cluster0.lm6iyo2.mongodb.net/?retryWrites=true&w=majority');
+
+    //you can connect to your local db like this
+    // const db = await mongoose.connect('mongodb://127.0.0.1:27017/yourDBname');
+    
     module.exports = db;
 }
 
